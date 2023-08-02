319772ce9b2d1ce6b02704f27d090261287600da
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-08-02 11:05:37 +0100

    initial version with routing, broken

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home'
+import { styled } from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <AppPage>
+      <h1>App Page</h1>
+      <BrowserRouter>
+        <NavBar>
+          <Link to="/">Home</Link>
 
-    </div>
+        </NavBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </AppPage>
   );
 }
+
+const AppPage = styled.nav`
+    display: flex;
+`;
+
+const NavBar = styled.nav`
+    display: flex;
+    width: 50%;
+    align-items: space-between;
+    justify-content: space-around;
+    
+`;
 
 export default App;
